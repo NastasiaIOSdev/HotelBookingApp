cd446dc6a1ca90c2aed2d85d46d0e75687cc96c8
AnastasiiaLarina <laranweb@gmail.com>
2024-01-15 15:07:46 +0500

    Start feature-1

---
+++ BookingApp/Assets/Colors.swift
@@ -0,0 +1,8 @@
+//
+//  Colors.swift
+//  BookingApp
+//
+//  Created by Анастасия Ларина on 15.01.2024.
+//
+
+import Foundation
---
+++ BookingApp/Assets/Fonts.swift
@@ -0,0 +1,8 @@
+//
+//  Fonts.swift
+//  BookingApp
+//
+//  Created by Анастасия Ларина on 15.01.2024.
+//
+
+import Foundation
