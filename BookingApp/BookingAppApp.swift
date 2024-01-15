1fdbecd5ab3c259d0e86098801aeec8b0a046653
AnastasiiaLarina <laranweb@gmail.com>
2024-01-15 14:57:32 +0500

    creating a project

---
+++ BookingApp/BookingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookingAppApp.swift
+//  BookingApp
+//
+//  Created by Анастасия Ларина on 15.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct BookingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
