1208351c146cd4072190ca1d0fa717c39df30b41
AnastasiiaLarina <laranweb@gmail.com>
2024-01-15 16:06:31 +0500

    Adding font, colors, assets

--- BookingApp/Assets/Fonts.swift
+++ BookingApp/Assets/Fonts.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+import SwiftUI
+
+enum Fonts {
+    case sf12
+    case sf14
+    case sf16
+    case sf17
+    case sf22
+    case sf30
+    
+    var font: UIFont? {
+        switch self {
+        case .sf12:
+            return UIFont(name: "SFProDisplay-Regular", size: 12)
+        case .sf14:
+            return UIFont(name: "SFProDisplay-Regular", size: 14)
+        case .sf16:
+            return UIFont(name: "SFProDisplay-Regular", size: 16)
+        case .sf17:
+            return UIFont(name: "SFProDisplay-Regular", size: 17)
+        case .sf22:
+            return UIFont(name: "SFProDisplay-Regular", size: 22)
+        case .sf30:
+            return UIFont(name: "SFProDisplay-Regular", size: 30)
+        }
+    }
+}
